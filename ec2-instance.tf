3f48008fbe9931d67d8bdc40eb9ec152854c6c9e
Saideep Chakilam <saideep.chakilam@cloud4c.com>
2024-07-16 17:15:28 +0530

    app user data apache

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,20 +1,3 @@
-#terrafrom setting block
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      #version = "~> 5.0"
-      # required in production level
-    }
-  }
-}
-
-
-
-# provider block
-provider "aws" {
-  region = "us-east-1"
-}
 
 
 # resource block
