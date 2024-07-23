1ac11e541854ca25551636b58b3761f30daf7688
Saideep Chakilam <saideep.chakilam@cloud4c.com>
2024-07-23 12:21:15 +0530

    looping commit

--- terraform2/a4.ec2.tf
+++ terraform2/a4.ec2.tf
@@ -4,8 +4,15 @@ resource "aws_instance" "terraform2_instance" {
   user_data     = file("${path.module}/app1-install.sh")
   ### any resource which accepts multple value, we should use square bracket or array
   vpc_security_group_ids = [aws_security_group.vpc_ssh.id, aws_security_group.vpc_http.id]
+  count = 2
+
   tags = {
-    "name" = "ec2_saideep"
+    "name" = "ec2_saideep-${count.index}"
   }
 
+}
+
+resource "aws_iam_user" "iamuser" {
+  count = 2
+  name = "sai-${count.index}"
 }
