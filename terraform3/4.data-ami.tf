fcf38a5030b6b53eb0c6f200c51d0263026afa2e
Saideep Chakilam <saideep.chakilam@cloud4c.com>
2024-08-01 12:56:29 +0530

    az availablity for instance type

--- terraform3/4.data-ami.tf
+++ terraform3/4.data-ami.tf
@@ -1,14 +1,51 @@
-data "aws_ec2_instance_type_offering" "inst_type" {
+# Fetch all availability zones that do not require opt-in
+data "aws_availability_zones" "my_zones" {
+  filter {
+    name = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+# Fetch the instance type offering in each availability zone
+data "aws_ec2_instance_type_offerings" "inst_type" {
+  for_each = toset(data.aws_availability_zones.my_zones.names)
   filter {
     name = "instance-type"
-    values = [ "t2.micro" ]
+    values = ["t3.large"]
   }
-
   filter {
     name = "location"
-    values = ["us-east-1e"]
-    #values = ["us-east-1a"]
+    values = [each.key]
   }
-
   location_type = "availability-zone"
 }
+
+# Output all availability zones
+output "o1" {
+  value = data.aws_availability_zones.my_zones.names
+}
+
+output "k" {
+  value = data.aws_ec2_instance_type_offerings.inst_type
+}
+
+
+#above output displays the all those az available
+
+output "o2" {
+  value = {
+    for az,details in data.aws_ec2_instance_type_offerings.inst_type:
+    az => details.instance_types if length(details.instance_types) != 0
+  }
+}
+
+output "o3" {
+  value = keys({
+    for az,details in data.aws_ec2_instance_type_offerings.inst_type:
+    az => details.instance_types if length(details.instance_types) != 0
+  })
+}
+
+
+
+#above output displays the only those az which supports my instance
--- terraform3/a5.output.tf
+++ terraform3/a5.output.tf
@@ -1,9 +1,9 @@
 /*output "instance_publicip" {
   description = "instance ip"
   value = aws_instance.terraform2_instance.public_ip
-}*/
+}
 
 
 output "ins_ava" {
   value = data.aws_ec2_instance_type_offering.inst_type.instance_type
-}
+}*/
